f8237ca6046fe48c8a978da1855ec2a18536d35b
Mitzael17 <80062221+Mitzael17@users.noreply.github.com>
2023-05-06 14:25:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const timerEl = document.querySelector('span');
+
+const createTimerAnimator = () => {
+
+  let timer;
+
+  return (timestamp) => {
+
+    clearInterval(timer);
+    makeStepTimer();
+
+    timer = setInterval( () => {
+
+      makeStepTimer();
+
+    }, 1000)
+
+    function makeStepTimer() {
+
+      if(timestamp === -1) {
+
+        clearInterval(timer);
+        return;
+
+      }
+
+      const seconds = timestamp % 60;
+      const minutes = (timestamp - seconds) / 60 % 60;
+      const hours = (timestamp - minutes * 60 - seconds) / 3600;
+
+      timerEl.innerHTML = `
+        ${hours > 100 ? hours : `0${hours}`.slice(-2)} :
+        ${`0${minutes}`.slice(-2)} :
+        ${`0${seconds}`.slice(-2)}
+      `;
+
+      timestamp--;
+
+    }
+
+  };
+};
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener('input', () => {
+
+  inputEl.value = inputEl.value.replace(/\D/g, '');
+
+});
+
+buttonEl.addEventListener('click', () => {
+  const seconds = Number(inputEl.value);
+
+  animateTimer(seconds);
+
+  inputEl.value = '';
+});
